055e6d6cac27f938018f645f9be9f8a7514142af
Clive Fuentebella <clivefuentebella@gmail.com>
2019-01-20 01:04:46 +0800

    Remove tooltip to one anchor in index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
           Be in part of the venue for intellectual, academic, and multidisciplinary interchanges.
         </p>
         <a class="uk-margin-top uk-margin-large-bottom" href="register" id="frontliner-header-register">Go to Registration</a><br>
-        <small class="uk-margin-small-bottom">If you had paid your accounts already and had registered, upload your slip <a href="http://www.feualabang.edu.ph/asaihl/#about" uk-tooltip="title: About the 2019 ASAIHL International Conference" id="frontliner-header-about">here</a>.<br>
+        <small class="uk-margin-small-bottom">If you had paid your accounts already and had registered, upload your slip <a href="http://www.feualabang.edu.ph/asaihl/#about" id="frontliner-header-about">here</a>.<br>
         </small>
         <small class="uk-margin-small-bottom">If you want to know more about ASAIHL, you can click <a href="http://www.feualabang.edu.ph/asaihl/#about" uk-tooltip="title: About the 2019 ASAIHL International Conference" id="frontliner-header-about">here</a>. </small>
       </div>
