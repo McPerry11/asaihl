b1f501209ed459afbbd9df49a5c86b581f65c84f
Clive Fuentebella <clivefuentebella@gmail.com>
2019-01-20 22:22:09 +0800

    Make settings commented to be hidden in user interface

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -3,29 +3,29 @@
     <ul class="uk-navbar-nav">
       <li class="uk-margin-left">
         <a href="dashboard">
-          <span class="uk-hidden@m"><i class="fas fa-columns"></i>&nbsp;</span>
+          <span class="uk-hidden@m"><i class="fas fa-columns"></i></span>
           <span class="uk-visible@m">Dashboard</span>
         </a>
       </li>
       <li>
         <a href="logs">
-          <span class="uk-hidden@m"><i class="far fa-list-alt"></i>&nbsp;</span>
+          <span class="uk-hidden@m"><i class="far fa-list-alt"></i></span>
           <span class="uk-visible@m">Logs</span>
         </a>
       </li>
-      <li>
+      <!-- <li>
         <a href="#">
-          <span class="uk-hidden@m"><i class="fas fa-sliders-h"></i>&nbsp;</span>
+          <span class="uk-hidden@m"><i class="fas fa-sliders-h"></i></span>
           <span class="uk-visible@m">Settings</span>
         </a>
-      </li>
+      </li> -->
     </ul>
   </div>
   <div class="uk-navbar-right">
     <ul class="uk-navbar-nav">
       <li class="uk-margin-right">
         <a href="logout">
-          <span class="uk-hidden@m"><i class="fas fa-sign-out-alt"></i>&nbsp;</span>
+          <span class="uk-hidden@m"><i class="fas fa-sign-out-alt"></i></span>
           <span class="uk-visible@m">Sign Out</span>
         </a>
       </li>
