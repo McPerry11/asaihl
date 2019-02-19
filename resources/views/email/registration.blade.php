8fa6a1d7127efe16e736d47d9086da7eae83a14c
Clive Fuentebella <clivefuentebella@gmail.com>
2019-02-19 18:40:15 +0800

    Add bank name

--- resources/views/email/registration.blade.php
+++ resources/views/email/registration.blade.php
@@ -3,14 +3,14 @@ Greetings from ASAIHL Staff,
 Thank you for participating in the 2019 ASAIHL International Conference! Please confirm your participation by paying your registration fee amounting to {{ $profile->citizenship == 'LOCAL' ? '₱' : '$' }}{{ $price * ($count + 1) }}.
 <br><br>
 You can pay the participation fee through this account: <br>
+Bank name: Banco De Oro (BDO)
 Account Name: <strong>Association of Southeast Asian Institution of Higher Learning, Inc.</strong><br>
 @if($profile->citizenship == 'FOREIGN') 
   Dollar Account Number: <strong>100020254245</strong>
 @else
   Peso / Savings Account Number: <strong>000020177488</strong>
 @endif
-<br>
-Paypal: <strong>ueccssrnd</strong><br><br>
+<br><br>
 Please take note of your barcode number: <strong>{{ $profile->barcode }}</strong>.
 <br><br>
 <img src="{{ str_replace("\\", "", url(DNS1D::getBarcodePNGPath($profile->barcode, "C39+"))) }}" alt="barcode">
