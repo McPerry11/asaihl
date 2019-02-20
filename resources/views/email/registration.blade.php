13d3c8f388caeaa7fd0441e2b73f61ef89a56ee9
neonspectrum <youngskymann@gmail.com>
2019-02-20 21:38:49 +0800

    Add number format

--- resources/views/email/registration.blade.php
+++ resources/views/email/registration.blade.php
@@ -1,11 +1,11 @@
 Greetings from ASAIHL Staff,
 <br><br>
-Thank you for participating in the 2019 ASAIHL International Conference! Please confirm your participation by paying your registration fee amounting to {{ $profile->citizenship == 'LOCAL' ? '₱' : '$' }}{{ $price * ($count + 1) }}.
+Thank you for participating in the 2019 ASAIHL International Conference! Please confirm your participation by paying your registration fee amounting to {{ $profile->citizenship == 'LOCAL' ? '₱' : '$' }}{{ number_format($price * ($count + 1), 2, ".", ",") }}.
 <br><br>
 You can pay the participation fee through this account: <br>
 Bank name: Banco De Oro (BDO)
 Account Name: <strong>Association of Southeast Asian Institution of Higher Learning, Inc.</strong><br>
-@if($profile->citizenship == 'FOREIGN') 
+@if($profile->citizenship == 'FOREIGN')
   Dollar Account Number: <strong>100020254245</strong>
 @else
   Peso / Savings Account Number: <strong>000020177488</strong>
