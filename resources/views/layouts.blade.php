2f0e25c4100955b59556515cb4311bb6bf298acf
neonspectrum <youngskymann@gmail.com>
2019-02-20 21:15:19 +0800

    Update favicon

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -10,12 +10,16 @@
 
   <title>ASAIHL International Conference</title>
 
+  <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+  <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
   <link href="https://fonts.googleapis.com/css?family=Abel|Montserrat|Ubuntu+Mono" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/css/uikit.min.css" />
   <link rel="stylesheet" href="https://unpkg.com/simplebar@latest/dist/simplebar.css" />
   <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+
   @yield('links')
 </head>
 <body>
